1e2d2db8a8cfd1257dd3b3702de3ea09846055f2
JayarajDeveloper <jayaraj.j@manurevasolutions.net>
2024-01-19 12:58:56 +0530

    testing

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,3 +3,19 @@
 # Example deployment script
 echo "Deploying to production..."
 # Add your deployment commands here
+
+
+
+#!/bin/bash
+
+# Navigate to the project directory
+# cd /path/to/your/project
+
+# # Pull the latest changes from the staging branch
+# git pull origin staging
+
+# # Install dependencies
+# npm install
+
+# # Restart the Node.js application (replace this with your actual start command)
+# pm2 restart your-app
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 3009;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
